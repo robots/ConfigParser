2c3f99f02a03c365300584d4a1c68d82dfa66abc
unknown <VFiklik@.(none)>
2011-05-05 15:34:37 +0200

    Test class added

---
+++ MainTest.java
@@ -0,0 +1,59 @@
+import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class MainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		IniParser iniP = new IniParserImpl();
+		
+		String [] enumVal = {"A", "B" };
+		List<Boolean> listBool = new LinkedList<Boolean>();
+		listBool.add(true);
+		listBool.add(false);
+		
+		List<String> listEnum = new LinkedList<String>();
+		listEnum.add("A");
+		listEnum.add("B");
+		
+		List<Float> listFloat = new LinkedList<Float>();
+		listFloat.add(1.0f);
+		listFloat.add(2.0f);
+		
+		List<BigInteger> listBig = new LinkedList<BigInteger>();
+		listBig.add(new BigInteger("150"));
+		listBig.add(new BigInteger("151"));
+		
+		iniP.createEnumType("EnumT", enumVal);
+		
+		
+		IniSection section = iniP.addSection("Default");
+		section.defineOptBoolean("defBool", true);
+		try{
+			section.defineOptEnum("defEnum", "EnumT", "A");
+		}catch( Exception e) { System.out.println(e.toString()); }
+		section.defineOptFloat("defFloat", 1.0f);
+		section.defineOptListBoolean("defListBool", ':', listBool);
+		try{
+		section.defineOptListEnum("defListEnum", "EnumT", ':', listEnum);
+		}catch( Exception e) { System.out.println(e.toString()); }
+		section.defineOptListFloat("defListFloat", ',', listFloat);
+		section.defineOptListSigned("defListSigned", ':', listBig);
+		section.defineOptListString("defListString", ',', listEnum);
+		section.defineOptListUnsigned("defListUnsigned", ':', listBig);
+		section.defineOptSigned("defSigned", new BigInteger("-15"));
+		section.defineOptString("defString", "Ahoj");
+		section.defineOptUnsigned("defUnsigned", new BigInteger("20"));
+		
+		System.out.println("Default section created");
+		
+		IniVisitor visitor = new PrintVisitor();
+		iniP.accept(visitor);
+	}
+
+}
