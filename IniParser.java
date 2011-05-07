0ccfb5535d0d0417b4c2342ecfe558ea97cec99e
Michal Demin <michaldemin@gmail.com>
2011-05-07 13:57:44 +0200

    Add exceptions to [input|optput]stream operations.
    Signed-off-by: Michal Demin <michaldemin@gmail.com>

--- IniParser.java
+++ IniParser.java
@@ -1,20 +1,19 @@
-
-
 import java.io.*;
 import java.math.BigInteger;
 import java.util.List;
 
 
 public interface IniParser {
-	
+	public void setAttitude(ParserAttitude attitude);
+
 	// Reads ini config from file
 	public void readFile(String fileName) throws IOException;
 	public void writeFile(String fileName) throws IOException;
-	public void readStream(InputStream inStream);	
-	public void writeStream(OutputStream outStream);
+	public void readStream(InputStream inStream) throws IOException;
+	public void writeStream(OutputStream outStream) throws IOException;
 	public String writeString();
 	public void readString(String inString);
-	
+
 	public String getString(String sectionName, String option) throws BadTypeException;
 	public float getFloat(String sectionName, String option) throws BadTypeException;
 	public boolean getBoolean(String sectionName, String option) throws BadTypeException;
@@ -22,14 +21,14 @@ public interface IniParser {
 	public BigInteger getUnsigned(String sectionName, String option) throws BadTypeException;
 	public String getEnum(String sectionName, String option) throws BadTypeException;
 	public String getUntyped(String sectionName, String option) throws BadTypeException;
-	
+
 	public void setString(String sectionName, String option, String value);
 	public void setFloat(String sectionName, String option, float value);
 	public void setBoolean(String sectionName, String option, boolean value);
 	public void setSigned(String sectionName, String option, BigInteger value);
 	public void setUnsigned(String sectionName, String option, BigInteger value);
 	public void setEnum(String sectionName, String option, String enumName, String value);
-	
+
 	public IniOption defineOptString(String sectionName, String option);
 	public IniOption defineOptString(String sectionName, String option, String defaultValue);
 	public IniOption defineOptBoolean(String sectionName, String option);
@@ -55,15 +54,12 @@ public interface IniParser {
 	public IniOption defineOptListUnsigned(String sectionName, String option, char delimiter, List<BigInteger> defaultValue);
 	public IniOption defineOptListEnum(String sectionName, String option, String enumName, char delimiter);
 	public IniOption defineOptListEnum(String sectionName, String option, String enumName, char delimiter, List<String> defaultValue) throws Exception;
-	
+
 	public IniSection addSection(String sectionName);
 	public IniSection getSection(String sectionName);
-	
+
 	public void createEnumType(String enumName, String[] values);
 	public boolean isValidForEnum(String enumName, String value);
-	
-	public void accept (IniVisitor visitor);
-	
-	
 
+	public void accept (IniVisitor visitor);
 }
